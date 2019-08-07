5efbda7e9462f12fec2b25b6a033adb08cd8ef77
Andres Ccopa <Andres.ccopa@gestionysistemas.com>
2019-08-07 14:21:23 -0500

    first commit

---
+++ core/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from django.views.generic.base import TemplateView
+
+class HomePageView(TemplateView):
+    template_name = "core/home.html"
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = "Web Playground"
+        return context
+
+class SamplePageView(TemplateView):
+    template_name = "core/sample.html"

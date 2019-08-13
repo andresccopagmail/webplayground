300c2a62fe21deec50f5cc7861605cec0a41296d
Andres Ccopa <Andres.ccopa@gestionysistemas.com>
2019-08-13 12:14:16 -0500

    cambiar email

--- registration/views.py
+++ registration/views.py
@@ -1,4 +1,4 @@
-from .forms import UserCreationFormWithEmail, ProfileForm
+from .forms import UserCreationFormWithEmail, ProfileForm, EmailForm
 from django.views.generic import CreateView
 from django.views.generic.edit import UpdateView
 from django.utils.decorators import method_decorator
@@ -17,7 +17,7 @@ class SignUpView(CreateView):
 
     def get_form(self, form_class=None):
         form = super(SignUpView, self).get_form()
-        #Modificación en tiempo real
+        # Modificación en tiempo real
         form.fields['username'].widget = forms.TextInput(attrs={
             'class':'form-control mb-2',
             'placeholder':'Nombre de usuario'
@@ -47,3 +47,21 @@ class ProfileUpdate(UpdateView):
         profile, created = Profile.objects.get_or_create(user=self.request.user)
         return profile
 
+@method_decorator(login_required, name='dispatch')
+class EmailUpdate(UpdateView):
+    form_class = EmailForm
+    success_url = reverse_lazy('profile')
+    template_name = 'registration/profile_email_form.html'
+
+    def get_object(self):
+        # Recuperar el objeto que se va a editar
+        return self.request.user
+
+    def get_form(self, form_class=None):
+        form = super(EmailUpdate, self).get_form()
+        # Modificación en tiempo real
+        form.fields['email'].widget = forms.EmailInput(attrs={
+            'class':'form-control mb-2',
+            'placeholder':'Email'
+        })
+        return form
